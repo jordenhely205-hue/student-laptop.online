bf48b6355f6893116aac6ece141a7458775c8bb6
Abbas Malik <rittus528@gmail.com>
2026-05-11 14:49:03 +0500

    Final UI fix

--- postcss.config.mjs.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}, // Ye line add karna zaroori hai
-  },
-};
-
-export default config;
